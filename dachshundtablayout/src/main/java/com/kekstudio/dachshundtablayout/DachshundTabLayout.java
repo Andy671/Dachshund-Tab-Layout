1f77778a87964eff02b921fe181ec4cff05b2c3d
Andrii Kotliarenko <andreykotlaren@gmail.com>
2018-05-04 14:16:51 +0300

    Refactor variables

--- dachshundtablayout/src/main/java/com/kekstudio/dachshundtablayout/DachshundTabLayout.java
+++ dachshundtablayout/src/main/java/com/kekstudio/dachshundtablayout/DachshundTabLayout.java
@@ -10,13 +10,12 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
-import com.kekstudio.dachshundtablayout.indicators.AnimatedIndicatorType;
 import com.kekstudio.dachshundtablayout.indicators.AnimatedIndicatorInterface;
+import com.kekstudio.dachshundtablayout.indicators.AnimatedIndicatorType;
 import com.kekstudio.dachshundtablayout.indicators.DachshundIndicator;
 import com.kekstudio.dachshundtablayout.indicators.LineFadeIndicator;
 import com.kekstudio.dachshundtablayout.indicators.LineMoveIndicator;
@@ -31,22 +30,19 @@ public class DachshundTabLayout extends TabLayout implements ViewPager.OnPageCha
 
     private static final int DEFAULT_HEIGHT_DP = 6;
 
-    private int indicatorColor;
-    private int indicatorHeight;
+    private int mIndicatorColor;
+    private int mIndicatorHeight;
 
-    private int currentPosition;
+    private int mCurrentPosition;
 
-    private boolean centerAlign;
+    private boolean mCenterAlign;
 
-    private ViewPager viewPager;
-    private LinearLayout tabStrip;
+    private LinearLayout mTabStrip;
 
-    private AnimatedIndicatorType animatedIndicatorType;
+    private AnimatedIndicatorType mAnimatedIndicatorType;
+    private AnimatedIndicatorInterface mAnimatedIndicator;
 
-    private AnimatedIndicatorInterface animatedIndicator;
-
-
-    int startXLeft, endXLeft, startXCenter, endXCenter, startXRight, endXRight;
+    int mStartXLeft, mEndXLeft, mStartXCenter, mEndXCenter, mStartXRight, mEndXRight;
 
     public DachshundTabLayout(Context context) {
         this(context, null);
@@ -61,14 +57,14 @@ public class DachshundTabLayout extends TabLayout implements ViewPager.OnPageCha
 
         super.setSelectedTabIndicatorHeight(0);
 
-        tabStrip = (LinearLayout) super.getChildAt(0);
+        mTabStrip = (LinearLayout) super.getChildAt(0);
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.DachshundTabLayout);
 
-        this.indicatorHeight = a.getDimensionPixelSize(R.styleable.DachshundTabLayout_ddIndicatorHeight, HelperUtils.dpToPx(DEFAULT_HEIGHT_DP));
-        this.indicatorColor = a.getColor(R.styleable.DachshundTabLayout_ddIndicatorColor, Color.WHITE);
-        this.centerAlign = a.getBoolean(R.styleable.DachshundTabLayout_ddCenterAlign, false);
-        this.animatedIndicatorType = AnimatedIndicatorType.values()[a.getInt(R.styleable.DachshundTabLayout_ddAnimatedIndicator, 0)];
+        this.mIndicatorHeight = a.getDimensionPixelSize(R.styleable.DachshundTabLayout_ddIndicatorHeight, HelperUtils.dpToPx(DEFAULT_HEIGHT_DP));
+        this.mIndicatorColor = a.getColor(R.styleable.DachshundTabLayout_ddIndicatorColor, Color.WHITE);
+        this.mCenterAlign = a.getBoolean(R.styleable.DachshundTabLayout_ddCenterAlign, false);
+        this.mAnimatedIndicatorType = AnimatedIndicatorType.values()[a.getInt(R.styleable.DachshundTabLayout_ddAnimatedIndicator, 0)];
 
         a.recycle();
     }
@@ -77,7 +73,7 @@ public class DachshundTabLayout extends TabLayout implements ViewPager.OnPageCha
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
 
-        if(centerAlign) {
+        if (mCenterAlign) {
             View firstTab = ((ViewGroup) getChildAt(0)).getChildAt(0);
             View lastTab = ((ViewGroup) getChildAt(0)).getChildAt(((ViewGroup) getChildAt(0)).getChildCount() - 1);
             ViewCompat.setPaddingRelative(getChildAt(0), (getWidth() / 2) - (firstTab.getWidth() / 2), 0, (getWidth() / 2) - (lastTab.getWidth() / 2), 0);
@@ -87,8 +83,8 @@ public class DachshundTabLayout extends TabLayout implements ViewPager.OnPageCha
 
     }
 
-    private void setupAnimatedIndicator(){
-        switch (animatedIndicatorType) {
+    private void setupAnimatedIndicator() {
+        switch (mAnimatedIndicatorType) {
             case DACHSHUND:
                 setAnimatedIndicator(new DachshundIndicator(this));
                 break;
@@ -108,19 +104,19 @@ public class DachshundTabLayout extends TabLayout implements ViewPager.OnPageCha
     }
 
     public void setAnimatedIndicator(AnimatedIndicatorInterface animatedIndicator) {
-        this.animatedIndicator = animatedIndicator;
+        this.mAnimatedIndicator = animatedIndicator;
 
-        animatedIndicator.setSelectedTabIndicatorColor(indicatorColor);
-        animatedIndicator.setSelectedTabIndicatorHeight(indicatorHeight);
+        animatedIndicator.setSelectedTabIndicatorColor(mIndicatorColor);
+        animatedIndicator.setSelectedTabIndicatorHeight(mIndicatorHeight);
 
         invalidate();
     }
 
     @Override
     public void setSelectedTabIndicatorColor(@ColorInt int color) {
-        this.indicatorColor = color;
-        if (animatedIndicator != null) {
-            animatedIndicator.setSelectedTabIndicatorColor(color);
+        this.mIndicatorColor = color;
+        if (mAnimatedIndicator != null) {
+            mAnimatedIndicator.setSelectedTabIndicatorColor(color);
 
             invalidate();
         }
@@ -128,17 +124,17 @@ public class DachshundTabLayout extends TabLayout implements ViewPager.OnPageCha
 
     @Override
     public void setSelectedTabIndicatorHeight(int height) {
-        this.indicatorHeight = height;
-        if (animatedIndicator != null) {
-            animatedIndicator.setSelectedTabIndicatorHeight(height);
+        this.mIndicatorHeight = height;
+        if (mAnimatedIndicator != null) {
+            mAnimatedIndicator.setSelectedTabIndicatorHeight(height);
 
             invalidate();
         }
 
     }
 
-    public void setCenterAlign(boolean centerAlign){
-        this.centerAlign = centerAlign;
+    public void setCenterAlign(boolean centerAlign) {
+        this.mCenterAlign = centerAlign;
 
         requestLayout();
     }
@@ -154,10 +150,8 @@ public class DachshundTabLayout extends TabLayout implements ViewPager.OnPageCha
 
         //TODO
         if (viewPager != null) {
-            if (viewPager != this.viewPager) {
-                viewPager.removeOnPageChangeListener(this);
-                viewPager.addOnPageChangeListener(this);
-            }
+            viewPager.removeOnPageChangeListener(this);
+            viewPager.addOnPageChangeListener(this);
         }
     }
 
@@ -165,8 +159,8 @@ public class DachshundTabLayout extends TabLayout implements ViewPager.OnPageCha
     public void draw(Canvas canvas) {
         super.draw(canvas);
 
-        if (animatedIndicator != null)
-            animatedIndicator.draw(canvas);
+        if (mAnimatedIndicator != null)
+            mAnimatedIndicator.draw(canvas);
 
     }
 
@@ -178,56 +172,56 @@ public class DachshundTabLayout extends TabLayout implements ViewPager.OnPageCha
     public void onPageScrolled(final int position, final float positionOffset,
                                final int positionOffsetPixels) {
 
-        if ((position > currentPosition) || (position + 1 < currentPosition)) {
-            currentPosition = position;
+        if ((position > mCurrentPosition) || (position + 1 < mCurrentPosition)) {
+            mCurrentPosition = position;
         }
 
-        if (position != currentPosition) {
+        if (position != mCurrentPosition) {
 
-            startXLeft = (int) getChildXLeft(currentPosition);
-            startXCenter = (int) getChildXCenter(currentPosition);
-            startXRight = (int) getChildXRight(currentPosition);
+            mStartXLeft = (int) getChildXLeft(mCurrentPosition);
+            mStartXCenter = (int) getChildXCenter(mCurrentPosition);
+            mStartXRight = (int) getChildXRight(mCurrentPosition);
 
-            endXLeft = (int) getChildXLeft(position);
-            endXRight = (int) getChildXRight(position);
-            endXCenter = (int) getChildXCenter(position);
+            mEndXLeft = (int) getChildXLeft(position);
+            mEndXRight = (int) getChildXRight(position);
+            mEndXCenter = (int) getChildXCenter(position);
 
-            if (animatedIndicator != null) {
-                animatedIndicator.setIntValues(startXLeft, endXLeft,
-                                                startXCenter, endXCenter,
-                                                startXRight, endXRight);
-                animatedIndicator.setCurrentPlayTime((long) ((1 - positionOffset) * (int) animatedIndicator.getDuration()));
+            if (mAnimatedIndicator != null) {
+                mAnimatedIndicator.setIntValues(mStartXLeft, mEndXLeft,
+                        mStartXCenter, mEndXCenter,
+                        mStartXRight, mEndXRight);
+                mAnimatedIndicator.setCurrentPlayTime((long) ((1 - positionOffset) * (int) mAnimatedIndicator.getDuration()));
             }
 
         } else {
 
-            startXLeft = (int) getChildXLeft(currentPosition);
-            startXCenter = (int) getChildXCenter(currentPosition);
-            startXRight = (int) getChildXRight(currentPosition);
+            mStartXLeft = (int) getChildXLeft(mCurrentPosition);
+            mStartXCenter = (int) getChildXCenter(mCurrentPosition);
+            mStartXRight = (int) getChildXRight(mCurrentPosition);
 
-            if (tabStrip.getChildAt(position + 1) != null) {
+            if (mTabStrip.getChildAt(position + 1) != null) {
 
-                endXLeft = (int) getChildXLeft(position + 1);
-                endXCenter = (int) getChildXCenter(position + 1);
-                endXRight = (int) getChildXRight(position + 1);
+                mEndXLeft = (int) getChildXLeft(position + 1);
+                mEndXCenter = (int) getChildXCenter(position + 1);
+                mEndXRight = (int) getChildXRight(position + 1);
 
-            }else{
-                endXLeft = (int) getChildXLeft(position);
-                endXCenter = (int) getChildXCenter(position);
-                endXRight = (int) getChildXRight(position);
+            } else {
+                mEndXLeft = (int) getChildXLeft(position);
+                mEndXCenter = (int) getChildXCenter(position);
+                mEndXRight = (int) getChildXRight(position);
             }
 
-            if(animatedIndicator != null){
-                animatedIndicator.setIntValues(startXLeft, endXLeft,
-                                                startXCenter, endXCenter,
-                                                startXRight, endXRight);
-                animatedIndicator.setCurrentPlayTime((long) (positionOffset * (int) animatedIndicator.getDuration()));
+            if (mAnimatedIndicator != null) {
+                mAnimatedIndicator.setIntValues(mStartXLeft, mEndXLeft,
+                        mStartXCenter, mEndXCenter,
+                        mStartXRight, mEndXRight);
+                mAnimatedIndicator.setCurrentPlayTime((long) (positionOffset * (int) mAnimatedIndicator.getDuration()));
             }
 
         }
 
         if (positionOffset == 0) {
-            currentPosition = position;
+            mCurrentPosition = position;
         }
 
     }
@@ -236,33 +230,33 @@ public class DachshundTabLayout extends TabLayout implements ViewPager.OnPageCha
     public void onPageSelected(final int position) {
     }
 
-    public int getCurrentPosition(){
-        return currentPosition;
+    public int getCurrentPosition() {
+        return mCurrentPosition;
     }
 
     public float getChildXLeft(int position) {
-        if(tabStrip.getChildAt(position) != null)
-            return (tabStrip.getChildAt(position).getX());
+        if (mTabStrip.getChildAt(position) != null)
+            return (mTabStrip.getChildAt(position).getX());
         else
             return 0;
     }
 
     public float getChildXCenter(int position) {
-        if(tabStrip.getChildAt(position) != null)
-            return (tabStrip.getChildAt(position).getX() + tabStrip.getChildAt(position).getWidth() / 2);
+        if (mTabStrip.getChildAt(position) != null)
+            return (mTabStrip.getChildAt(position).getX() + mTabStrip.getChildAt(position).getWidth() / 2);
         else
             return 0;
     }
 
     public float getChildXRight(int position) {
-        if(tabStrip.getChildAt(position) != null)
-            return (tabStrip.getChildAt(position).getX() + tabStrip.getChildAt(position).getWidth());
+        if (mTabStrip.getChildAt(position) != null)
+            return (mTabStrip.getChildAt(position).getX() + mTabStrip.getChildAt(position).getWidth());
         else
             return 0;
     }
 
     public AnimatedIndicatorInterface getAnimatedIndicator() {
-        return animatedIndicator;
+        return mAnimatedIndicator;
     }
 
 
