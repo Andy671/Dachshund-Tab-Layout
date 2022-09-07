8492f6e0153aa591186f700be1278dd438601c2b
Hamza Rizwan <hamzarizwan243@gmail.com>
2022-09-07 16:18:45 +0530

    v0.3.3
    - Migrated to AndroidX
    - Added ViewPager2 support
    - Updated targetSdk and bumped old APIs

--- dachshundtablayout/src/main/java/com/kekstudio/dachshundtablayout/HelperUtils.java
+++ dachshundtablayout/src/main/java/com/kekstudio/dachshundtablayout/HelperUtils.java
@@ -8,13 +8,13 @@ import android.util.TypedValue;
  */
 
 public class HelperUtils {
-
+    
     public static int pxToDp(float px) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, px, Resources.getSystem().getDisplayMetrics());
     }
-
+    
     public static int dpToPx(int dp) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().getDisplayMetrics());
     }
-
+    
 }
